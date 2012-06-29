052ec66337f708be19633f5380d06351fe5beef7
Satoshi Egi <egisatoshi@gmail.com>
2012-06-29 15:55:07 +0900

    add compile-test.egi

--- hs-src/Language/Egison/Core.hs
+++ hs-src/Language/Egison/Core.hs
@@ -15,7 +15,7 @@ import Paths_egison
 
 -- |egison version number
 egisonVersion :: String
-egisonVersion = "2.1.0"
+egisonVersion = "2.1.1"
 
 -- |A utility function to display the egison console banner
 showBanner :: IO ()
