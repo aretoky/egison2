3f592778062ef7fe6ceee2c88ebc32733f918df2
Satoshi Egi <egisatoshi@gmail.com>
2012-07-13 18:19:33 +0900

    increase version number to 2.1.16

--- hs-src/Paths_egison.hs
+++ hs-src/Paths_egison.hs
@@ -2,7 +2,7 @@ module Paths_egison where
 import Data.Version
 
 version :: Version
-version = Version [2,1,15] []
+version = Version [2,1,16] []
 
 getDataFileName :: FilePath -> IO FilePath
 getDataFileName filename = return ("/home/egi/egison2/" ++ filename)
